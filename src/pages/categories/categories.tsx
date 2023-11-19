543e6c4074f3d49115b9983244563bf191b6cb22
adilrion <adilmahmoudrion@gmail.com>
2023-11-19 22:25:06 +0600

    cre

--- src/pages/categories/categories.tsx
+++ src/pages/categories/categories.tsx
@@ -41,18 +41,35 @@ const categories = [
         title: "T-shirt",
         link: "/t-shirt",
     },
+    {
+        img: "https://sportsworld.com.bd/wp-content/uploads/2021/12/Sports-T-Shirt-Deep-Navy-Blue-With-Ash-Texture-F.png",
+        title: "T-shirt",
+        link: "/t-shirt",
+    },
+    {
+        img: "https://sportsworld.com.bd/wp-content/uploads/2021/12/Sports-T-Shirt-Deep-Navy-Blue-With-Ash-Texture-F.png",
+        title: "T-shirt",
+        link: "/t-shirt",
+    },
+    {
+        img: "https://sportsworld.com.bd/wp-content/uploads/2021/12/Sports-T-Shirt-Deep-Navy-Blue-With-Ash-Texture-F.png",
+        title: "T-shirt",
+        link: "/t-shirt",
+    },
+    {
+        img: "https://sportsworld.com.bd/wp-content/uploads/2021/12/Sports-T-Shirt-Deep-Navy-Blue-With-Ash-Texture-F.png",
+        title: "T-shirt",
+        link: "/t-shirt",
+    },
 ];
 const Categories = () => {
     return (
         <section>
-            <div className="flex flex-wrap">
+            <p className="underline decoration-wavy">The quick brown fox</p>
+            <div className="flex flex-wrap bg-white shadow my-4 rounded divide-x divide-y">
                 {categories.map((data, index) => {
                     return (
-                        <Link
-                            to={data.link}
-                            key={index}
-                            className="shadow basis-1/6"
-                        >
+                        <Link to={data.link} key={index} className=" basis-1/6">
                             <img
                                 className="w-[200px] h-[200px] "
                                 src={data.img}
