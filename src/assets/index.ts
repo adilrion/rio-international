c149a17fba4f3aed1a9cde277c6b2ff26ecfb08b
adilrion <adilmahmoudrion@gmail.com>
2023-11-26 00:17:26 +0600

    design payment method

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,10 +2,30 @@ import logo from './logo/logo.png'
 import loginImage from './auth/loginPageImages.jpg'
 import regImgage from './auth/registration.jpg'
 import cl from './tem/c.png'
+import nagad from './tem/payment/Nagad-Logo.wine.svg'
+import payoneer from './tem/payment/Payoneer-Logo.wine.svg'
+import bkash from './tem/payment/bkash.svg'
+import worldpay from './tem/payment/Worldpay-Logo.wine.svg'
+import card from './tem/payment/card.png'
+import cashOnDelivery from './tem/payment/cash-on-delivery.png'
+import gpay from './tem/payment/google-pay.png'
+import paypal from './tem/payment/paypal.png'
+import stripe from './tem/payment/stripe.png'
+import visa from './tem/payment/visa.png'
 
 export const assets = {
     logo,
     loginImage,
     regImgage,
-    cl
+    cl,
+    nagad,
+    payoneer,
+    bkash,
+    worldpay,
+    card,
+    cashOnDelivery,
+    gpay,
+    paypal,
+    stripe,
+    visa
 };
--- src/components/paymentMethod/PaymentMethod.tsx
+++ src/components/paymentMethod/PaymentMethod.tsx
@@ -1,9 +1,35 @@
+import { assets } from '@/assets';
 import React from 'react';
 
+
+const paymentOptions = [
+    {image: assets.bkash, title: '', href: ''},
+    {image: assets.gpay, title: '', href: ''},
+    {image: assets.nagad, title: '', href: ''},
+    {image: assets.payoneer, title: '', href: ''},
+    {image: assets.worldpay, title: '', href: ''},
+    {image: assets.stripe, title: '', href: ''},
+    {image: assets.paypal, title: '', href: ''},
+    {image: assets.card, title: '', href: ''},
+    {image: assets.visa, title: '', href: ''},
+    {image: assets.cashOnDelivery, title: '', href: ''},
+    
+]
+
 const PaymentMethod = () => {
+
     return (
-        <div>
-            <h2>Payment Method</h2>
+        <div className='py-6'>
+            <h1 className="text-[26px] font-mono antialiased font-medium tracking-wide ">
+                Payment Method
+            </h1>
+            <div className="flex flex-wrap px-[15rem] gap-4 justify-center items-center">
+                {paymentOptions.map((data, index) => (
+                    <div key={index} className="w-[80px] h-[80px] flex justify-center items-start">
+                        <img src={data.image} alt="images" className='w-full h-full' />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
